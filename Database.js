cd03e8b30bbb180d3c7cae4fcdfdc01e1a49af7a
kobietdatten100-sketch <kobietdatten100@gmail.com>
2026-06-26 10:14:14 +0700

    Update Database.js

--- Database.js
+++ Database.js
@@ -1,32 +1,50 @@
 const fs = require("fs");
+const path = require("path");
 
-const path = "./data/users.json";
+const dataFolder = "./data";
+const dataFile = path.join(dataFolder, "users.json");
+
+// Tự tạo thư mục data nếu chưa có
+if (!fs.existsSync(dataFolder)) {
+    fs.mkdirSync(dataFolder);
+}
+
+// Tự tạo users.json nếu chưa có
+if (!fs.existsSync(dataFile)) {
+    fs.writeFileSync(dataFile, "{}", "utf8");
+}
 
 function loadData() {
-return JSON.parse(fs.readFileSync(path));
+    try {
+        return JSON.parse(fs.readFileSync(dataFile, "utf8"));
+    } catch {
+        return {};
+    }
 }
 
 function saveData(data) {
-fs.writeFileSync(path, JSON.stringify(data, null, 2));
+    fs.writeFileSync(dataFile, JSON.stringify(data, null, 2));
 }
 
 function createUser(id) {
-const data = loadData();
+    const data = loadData();
 
-if (!data[id]) {
-data[id] = {
-coins: 0,
-daily: 0
-};
+    if (!data[id]) {
+        data[id] = {
+            coins: 1000,     // Coin mặc định
+            daily: 0,
+            win: 0,
+            lose: 0
+        };
 
-saveData(data);
-}
+        saveData(data);
+    }
 
-return data[id];
+    return data[id];
 }
 
 module.exports = {
-loadData,
-saveData,
-createUser
+    loadData,
+    saveData,
+    createUser
 };
